b95dca9116f05b630f99d0faba106e8999c41c22
shapman lv <shapmanlv@outlook.com>
2023-08-10 01:32:54 +0800

    加入consul

--- src/gateway/apigateway/Program.cs
+++ src/gateway/apigateway/Program.cs
@@ -1,6 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.Provider.Polly;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+  .AddOcelot()
+  .AddConsul()
+  .AddPolly();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "hello world, I'm api gateway");
+await app.UseOcelot();
 
 app.Run();
