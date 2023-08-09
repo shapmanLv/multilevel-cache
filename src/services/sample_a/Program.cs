57c34c500f4947f9815161c25b389980f8222413
shapman lv <shapmanlv@outlook.com>
2023-08-09 23:05:55 +0800

    修改dockerfile

--- src/services/sample_a/Program.cs
+++ src/services/sample_a/Program.cs
@@ -1,14 +1,9 @@
-﻿using System.Net;
+﻿using common;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGet("/", () => "hello world, I'm a microservice");
-app.MapGet("/ip", async () => 
-    
-);
+app.MapGet("/ip", async () => await IpAddress.GetIpAsync());
+
 app.Run();
--- src/services/sample_b/Program.cs
+++ src/services/sample_b/Program.cs
@@ -1,7 +1,10 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using common;
+
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "hello world, I'm b microservice");
+app.MapGet("/ip", async () => await IpAddress.GetIpAsync());
 
 app.Run();
 
