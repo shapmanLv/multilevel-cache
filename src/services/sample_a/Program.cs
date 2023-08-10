1986cb1833aac4a8a94ab685d0899f48dd400257
shapman lv <shapmanlv@outlook.com>
2023-08-11 00:10:42 +0800

    配置服务发现与注册

--- src/services/sample_a/Program.cs
+++ src/services/sample_a/Program.cs
@@ -1,11 +1,32 @@
 using common;
+using Consul;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var ip = await IpAddress.GetIpAsync();
 
 app.MapGet("/", () => "hello world, I'm a microservice");
-app.MapGet("/ip", async () => await IpAddress.GetIpAsync());
+app.MapGet("/ip", () => ip);
 app.MapGet("/health", () => "I am fit");
+
+var consulClient = new ConsulClient(config => config.Address = new Uri("http://consul:8500"));
+var serviceId = Guid.NewGuid().ToString("N");
+await consulClient.Agent.ServiceRegister(new AgentServiceRegistration
+{
+    ID = serviceId,
+    Address = ip,
+    Name = "sample_a",
+    Port = 80,
+    Check = new AgentServiceCheck
+    {
+        Interval = TimeSpan.FromSeconds(10),
+        Timeout = TimeSpan.FromSeconds(5),
+        HTTP = $"http://{ip}/health",
+        DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(3),
+    }
+});
+app.Lifetime.ApplicationStopping.Register(() =>
+  consulClient.Agent.ServiceDeregister(serviceId));
 
 app.Run();
